0ab2f34be13a9d270019254ec092ede5d5d48e2b
SantiSaaUCB <santiago.saavedra@ucb.edu.bo>
2025-02-20 09:46:08 -0400

    feat: Retornar el mismo numero si no sigue una regla

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -2,6 +2,9 @@ import fizzbuzz from "./fizzbuzz.js"
 
 describe("FizzBuzz", () => {
      it("deberia retornar el mismo numero si el numero no sigue una regla", () => {
+          expect(fizzbuzz(1)).toEqual("1");
+     });
+     it("deberia retornar el mismo numero si el numero no sigue una regla", () => {
           expect(fizzbuzz(2)).toEqual("2");
      });
 });
